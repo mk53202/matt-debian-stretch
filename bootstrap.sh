06e56c2484a089736604e2179a2afda6a0be769b
Matt Koster <mattkoster@gmail.com>
2018-06-11 12:39:47 -0500

    Updated bootstrip.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y htop nginx curl net-tools ansible
-
-curl -sL https://deb.nodesource.com/setup_6.x | bash -
-apt-get install -y nodejs
-npm install npm --global
-
+apt-get install -y htop nginx curl net-tools
 curl --silent https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py
+
+# curl -sL https://deb.nodesource.com/setup_6.x | bash -
+# apt-get install -y nodejs
+# npm install npm --global
 
 # if ! [ -L /var/www ]; then
 #   rm -rf /var/www
 #   ln -fs /vagrant /var/www
 # fi
+
+echo alias ll=\'ls -lat\' >> ~/.bashrc
