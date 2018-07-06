1ce35e0c079a36132075422479ebe1fb5a1ae672
Matt Koster <mattkoster@gmail.com>
2018-07-06 14:29:50 -0500

    Updated bootstrap.sh to modularize builds.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-# Pick 'n choose build your box
+# Pick 'n choose, build your box
 
-# Common stuff
+# - Install common stuff
 apt-get update
 apt-get install -y htop git curl net-tools
 apt-get install -y apt-transport-https
 
-# << Web Server >>
+# - Install webserver stuff
 # apt-get install -y nginx
 # if ! [ -L /var/www ]; then
 #   rm -rf /var/www
@@ -15,21 +15,37 @@ apt-get install -y apt-transport-https
 #   ln -s /vagrant/www /var/www/html
 # fi
 
-# Base Python stuff
+# - Install Python stuff
 # curl --silent https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 # python get-pip.py
 # apt-get install -y python-dev
 
+# - Install Nodejs
 # curl -sL https://deb.nodesource.com/setup_6.x | bash -
 # apt-get install -y nodejs
 # npm install npm --global
+
+# - Install java JDK
+# https://www.digitalocean.com/community/tutorials/how-to-install-java-with-apt-get-on-debian-8
+echo Install Java JDK
+apt-get install -y default-jdk
+
+# - Install elasticsearch 6.3.1 manually, requires JAVA JDK/JRE
+# Source doc: https://www.elastic.co/guide/en/elasticsearch/reference/current/deb.html
+# Test es: curl -X GET "localhost:9200/"
+echo Install elasticsearch
+wget --quiet https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.3.1.deb
+dpkg --install elasticsearch-6.3.1.deb
+systemctl daemon-reload
+systemctl enable elasticsearch.service
+systemctl start elasticsearch.service
 
 
 echo Updating bash_aliases
 #
 echo alias ll=\'ls -lat\' >> /home/vagrant/.bash_aliases
 
-echo Other stuff and things
+echo Run stuff and things
 #
 /usr/bin/env
 /sbin/ifconfig -a | grep 'inet '
--- scripts/install_elastic.sh
+++ scripts/install_elastic.sh
@@ -1,7 +1,11 @@
 # https://www.elastic.co/guide/en/elasticsearch/reference/current/deb.html
 
-sudo apt-get install apt-transport-https
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
-sudo apt-get update && sudo apt-get install elasticsearch
-sudo apt-get install elasticsearch
+# wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+# echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
+# sudo apt-get update && sudo apt-get install elasticsearch
+# sudo apt-get install elasticsearch
+
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.3.1.deb
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.3.1.deb.sha512
+shasum -a 512 -c elasticsearch-6.3.1.deb.sha512
+sudo dpkg --log=/tmp/es.log --install elasticsearch-6.3.1.deb
