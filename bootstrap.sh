f78433f62f242bd86b61ee8daef180ff8edd6517
Matt Koster <mattkoster@gmail.com>
2018-07-06 19:19:30 -0500

    Fixed quotes.

--- bootstrap.sh
+++ bootstrap.sh
@@ -38,6 +38,7 @@ wget --quiet https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-
 dpkg --install elasticsearch-6.3.1.deb
 echo 'cluster.name: koster-kluster' >> /etc/elasticsearch/elasticsearch.yml
 echo 'network.host: 0.0.0.0' >> /etc/elasticsearch/elasticsearch.yml
+echo 'discovery.zen.ping.unicast.hosts: ["10.0.1.215", "10.0.1.216"]' >> /etc/elasticsearch/elasticsearch.yml
 systemctl daemon-reload
 systemctl enable elasticsearch.service
 systemctl start elasticsearch.service
