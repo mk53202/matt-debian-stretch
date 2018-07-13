a10dbf1536e67a8662d0ee075ac4f8c0eec23975
Matt Koster <mattkoster@gmail.com>
2018-07-12 20:27:28 -0500

    Initial Docker install and checkout.

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,6 +20,21 @@ apt-get install -y apt-transport-https
 # python get-pip.py
 # apt-get install -y python-dev
 
+# - Install Docker
+# https://www.itzgeek.com/how-tos/linux/debian/how-to-install-docker-on-debian-9.html
+# https://www.itzgeek.com/how-tos/linux/how-to-build-docker-images-with-dockerfile.html
+# https://hub.docker.com/_/nginx/
+echo ############ Install Docker
+apt-get install -y apt-transport-https ca-certificates wget software-properties-common
+wget --quiet https://download.docker.com/linux/debian/gpg
+apt-key add gpg
+echo "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee -a /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-cache policy docker-ce
+apt-get -y install docker-ce
+systemctl enable docker
+docker run hello-world
+
 # - Install Nodejs
 # curl -sL https://deb.nodesource.com/setup_6.x | bash -
 # apt-get install -y nodejs
@@ -27,22 +42,22 @@ apt-get install -y apt-transport-https
 
 # - Install java JDK
 # https://www.digitalocean.com/community/tutorials/how-to-install-java-with-apt-get-on-debian-8
-echo Install Java JDK
-apt-get install -y default-jdk
+# echo Install Java JDK
+# apt-get install -y default-jdk
 
 # - Install elasticsearch 6.3.1 manually, requires JAVA JDK/JRE
 # Source doc: https://www.elastic.co/guide/en/elasticsearch/reference/current/deb.html
 # Test es: curl -X GET "localhost:9200/"
 
-echo Fetch, install, and configure elasticsearch
-wget --quiet https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.3.1.deb
-dpkg --install elasticsearch-6.3.1.deb
-echo 'network.host: 0.0.0.0' >> /etc/elasticsearch/elasticsearch.yml
-# echo 'cluster.name: koster-kluster' >> /etc/elasticsearch/elasticsearch.yml
-# echo 'discovery.zen.ping.unicast.hosts: ["10.0.1.215", "10.0.1.216"]' >> /etc/elasticsearch/elasticsearch.yml
-systemctl daemon-reload
-systemctl enable elasticsearch.service
-systemctl start elasticsearch.service
+# echo Fetch, install, and configure elasticsearch
+# wget --quiet https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.3.1.deb
+# dpkg --install elasticsearch-6.3.1.deb
+# echo 'network.host: 0.0.0.0' >> /etc/elasticsearch/elasticsearch.yml
+# # echo 'cluster.name: koster-kluster' >> /etc/elasticsearch/elasticsearch.yml
+# # echo 'discovery.zen.ping.unicast.hosts: ["10.0.1.215", "10.0.1.216"]' >> /etc/elasticsearch/elasticsearch.yml
+# systemctl daemon-reload
+# systemctl enable elasticsearch.service
+# systemctl start elasticsearch.service
 
 
 echo Updating bash_aliases
